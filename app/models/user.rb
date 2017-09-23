57262f2d50c982bb8fc783bd73dc24c470e0f124
jnickell <nickell75@yahoo.com>
2017-09-22 17:02:00 -0700

    Adds functional user sign up and login pages

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+
+	validates :username, :email, presence: true
+	validates :username, :email, uniqueness: true
+
+	has_secure_password
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
+
+  root 	'movies#index'
+
+  get 		'login', to: 'sessions#new'
+  post 		'login', to: 'sessions#create'
+  delete  'logout', to: 'sessions#delete'
+
+  get 	'signup', to: 'users#new'
+  post 	'signup', to: 'users#create'
+
+  get 	'profile/:id', to: 'users#show'
 end
